ecc246122b3b448b3c2cf0cb3ebc29573fc3a24c
mishina naoya <msnyxx811@gmail.com>
2023-06-20 22:27:11 +0900

    update

--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -22,12 +22,12 @@
                       @endif  
                       </div>
                       <div class="text-xl">{{ $shop->name }}</div>
-                      @if(empty($shop->filename))
+                      {{-- @if(empty($shop->filename))
                         <img src="{{asset('images/no_image.jpg')}}">
                       @else
                         <img src="{{asset('storage/shops/' . $shop->filename)}}"> 
-                      @endif
-                      {{-- <x-thumbnail :filename="$shop->filename" type="shops" /> --}}
+                      @endif --}}
+                      <x-thumbnail :filename="$shop->filename" type="shops" />
 
                     </div>
                     </a>
