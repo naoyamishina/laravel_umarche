56434a5d77c090841889ad574126310248a77729
mishina naoya <msnyxx811@gmail.com>
2023-06-28 22:39:25 +0900

    キュージョブ

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail; 
 use App\Mail\TestMail;
+use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -32,8 +33,12 @@ class ItemController extends Controller
 
     public function index(Request $request)
     {
-        Mail::to('test@example.com')
-        ->send(new TestMail());
+        // 同期処理 
+        // Mail::to('test@example.com')
+        // ->send(new TestMail());
+
+        // キューにジョブを入れて処理(非同期) 
+        SendThanksMail::dispatch();
 
         $categories = PrimaryCategory::with('secondary')
         ->get();
