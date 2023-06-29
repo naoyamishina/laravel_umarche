dfd4c76db915288d9ed058ba3c123c1272ec748c
mishina naoya <msnyxx811@gmail.com>
2023-06-29 21:49:20 +0900

    メール修正

--- app/Jobs/SendThanksMail.php
+++ app/Jobs/SendThanksMail.php
@@ -8,26 +8,21 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Mail; 
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestMail;
 use App\Mail\ThanksMail;
-
 
 class SendThanksMail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public $user; 
-    Public $products;
+    public $products;
+    public $user;
 
-    /**
-     * Create a new job instance.
-     *
-     * @return void
-     */
-    public function __construct($user, $products)
+    public function __construct($products, $user)
     {
+        $this->products = $products;
         $this->user = $user;
-        $this->products = $products; 
     }
 
     /**
@@ -37,7 +32,8 @@ class SendThanksMail implements ShouldQueue
      */
     public function handle()
     {
+        // Mail::to('test@example.com')->send(new TestMail());
         Mail::to($this->user)
-        ->send(new ThanksMail($this->user, $this->products));
+        ->send(new ThanksMail($this->products, $this->user));
     }
 }
